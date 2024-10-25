0ea54a2c30406762983f4ce94a604b1e222ae8d7
ella-an2 <ella.an@mths.ca>
2024-10-25 10:58:02 -0400

    fixed the comments

--- main.ts
+++ main.ts
@@ -5,15 +5,15 @@
  * This program lights up depending on the distance
 */
 
-//variable
+// variable
 let distanceToObject: number = 0
 let neopixelStrip: neopixel.Strip = null
 
-//setup
+// setup
 basic.showIcon(IconNames.Happy)
 basic.clearScreen()
 
-//Turning off the lights
+// Turning off the lights
 neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
@@ -22,17 +22,17 @@ neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.show()
 basic.showIcon(IconNames.Happy)
 
-//When button a is pressed
+// When button a is pressed
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
-     //To get the distance
+     // to get the distance
     distanceToObject = sonar.ping(
         DigitalPin.P1,
         DigitalPin.P2,
         PingUnit.Centimeters
     )
     
-    //When distance is less than 10
+    // When distance is less than 10
     if (distanceToObject < 10 ) {
         neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
         neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
@@ -41,9 +41,7 @@ input.onButtonPressed(Button.A, function () {
         neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
         neopixelStrip.show()
         basic.showIcon(IconNames.Happy)
-    }
-    //When distance is more or equal to 10
-    else {
+    } else {
         neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
         neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
         neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
